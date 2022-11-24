6ae0729c3e23ad4a813c84a8e5f40d226a3e1d5b
Federico Belvedere <federico.belvedere@gmail.com>
2022-11-24 12:05:18 -0300

    phoenix

--- b1/lib/b1_web/controllers/hangman_controller.ex
+++ b1/lib/b1_web/controllers/hangman_controller.ex
@@ -10,16 +10,31 @@ defmodule B1Web.HangmanController do
     tally = Hangman.tally(game)
     #put_session(conn, :game, game)  ---->
     conn = put_session(conn, :game, game)
-    render(conn, "game.html", tally: tally)
+    #render(conn, "game.html", tally: tally)
+    redirect(conn, to: Routes.hangman_path(conn, :show))
   end
 
   def update(conn, params) do
-    #raise inspect(params)
     guess = params["make_move"]["guess"]
-    game = get_session(conn, :game)
-    tally = Hangman.make_move(game, guess)
-    conn = put_in(conn.params["make_move"]["guess"], "")
-    render(conn, "game.html", tally: tally)
+
+    put_in(conn.params["make_move"]["guess"], "")
+    |> get_session(:game)
+    |> Hangman.make_move(guess)
+    #raise inspect(params)
+    #guess = params["make_move"]["guess"]
+    #game = get_session(conn, :game)
+    #tally = Hangman.make_move(game, guess)
+    #conn = put_in(conn.params["make_move"]["guess"], "")
+    #render(conn, "game.html", tally: tally)
+    redirect(conn, to: Routes.hangman_path(conn, :show))
+  end
+
+  def show(conn, _param) do
+    tally =
+      conn
+      |> get_session(:game)
+      |> Hangman.tally()
+      render(conn, "game.html", tally: tally)
   end
 
 end
